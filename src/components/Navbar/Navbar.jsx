265593000425f92b7fc93b35e1899adc67454856
ansh4223 <ak210073@gmail.com>
2024-06-03 16:17:41 +0530

    Feat: Dark mode features

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BiSolidMoon, BiSolidSun } from "react-icons/bi";
 
 const NavLinks = [
   {
@@ -22,9 +23,9 @@ const NavLinks = [
     link: "/#booking",
   },
 ];
-const Navbar = () => {
+const Navbar = ({theme, setTheme}) => {
   return (
-    <nav className="shadow-sm">
+    <nav className="shadow-md bg-white dark:bg-dark dark:text-white duration-300">
       <div className="container">
         <div className="flex justify-between items-center">
           <div>
@@ -35,12 +36,25 @@ const Navbar = () => {
               {NavLinks.map((data) => (
                 <li key={data.id}
                 className="py-4">
-                  <a className="inline-block py-2 hover:border-b-2 hover:text-primary hover:border-primary transition-colors duration-500 text-lg font-medium" href={data.link}>
+                  <a className="py-2 hover:border-b-2 hover:text-primary hover:border-primary transition-colors duration-500 text-lg font-medium" href={data.link}>
                     {data.name}
                   </a>
                 </li>
               ))}
             </ul>
+          </div>
+          <div>
+          {theme === "dark" ? (
+            <BiSolidSun
+            onClick={() => setTheme("light")}
+            className="text-2xl"/>
+            ):(
+              <BiSolidMoon 
+              onClick={() => setTheme("dark")}
+              className="text-2xl"/>
+            )
+          }
+            
           </div>
         </div>
       </div>
