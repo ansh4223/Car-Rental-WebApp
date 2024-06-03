9b2f7807053ae2bf318616b77ca8100b1f6d7c99
ansh4223 <ak210073@gmail.com>
2024-06-03 14:31:41 +0530

    Add: navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React from "react";
 
+const NavLinks = [
+  {
+    id: 1,
+    name: "HOME",
+    link: "/#",
+  },
+  {
+    id: 2,
+    name: "CARS",
+    link: "/#cars",
+  },
+  {
+    id: 3,
+    name: "ABOUT",
+    link: "/#about",
+  },
+  {
+    id: 4,
+    name: "BOOKING",
+    link: "/#booking",
+  },
+];
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
-  )
-}
+    <nav className="shadow-sm">
+      <div className="container">
+        <div className="flex justify-between items-center">
+          <div>
+            <h1 className="text-3xl font-bold font-serif">Car Rental</h1>
+          </div>
+          <div className="hidden md:block">
+            <ul className="flex items-center gap-8">
+              {NavLinks.map((data) => (
+                <li key={data.id}
+                className="py-4">
+                  <a className="inline-block py-2 hover:border-b-2 hover:text-primary hover:border-primary transition-colors duration-500 text-lg font-medium" href={data.link}>
+                    {data.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
