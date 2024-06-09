c63759b0fb5b45318c86da2386005a726abbcb63
ansh4223 <ak210073@gmail.com>
2024-06-09 23:12:21 +0530

    Feat: Added get started button

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,23 +1,31 @@
-import React from 'react'
-import carPng from "../../assets/car.png"
+import React from "react";
+import carPng from "../../assets/car.png";
 
 const Hero = () => {
   return (
-    <div className="dark:bg-black dark:text-white duration-300">
-    <div className="container min-h-[620px] flex">
-        <div className='grid grid-cols-1 sm:grid-cols-2 place-items-center'>
-            <div className='order-1 sm:order-2'>
-                <img src={carPng} alt="" />
-            </div>
-            <div  className='order-2 sm:order-1'>
-                <p className='text-primary'>Effortless</p>
-                <h1>Car Rental</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo</p>
-            </div>
+    <div className="dark:bg-black dark:text-white duration-300 relative -z-50">
+      <div className="container min-h-[620px] flex">
+        <div className="grid grid-cols-1 sm:grid-cols-2 place-items-center">
+          <div className="order-1 sm:order-2">
+            <img src={carPng} alt=""
+            className="relative -z-10 max-h-[600px] sm:scale-125 drop-shadow-[2px_20px_6px_rgba(0,0,0,0.50)]" />
+          </div>
+          <div className="order-2 sm:order-1 space-y-5 sm:pr-32">
+            <p className="text-primary text-2xl font-serif">Effortless</p>
+            <h1 className="tex-5xl lg:text-7xl font-semibold font-serif">
+              Car Rental
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+            </p>
+            <button className="btn bg-primary text-black px-6 py-2 rounded-md hover:bg-primary/80 duration-300">
+              Get Started
+            </button>
+          </div>
         </div>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
